bc48900600faa58ad8d6dfaca58ec05eb7522f02
Ed Overton <e.overton@gmail.com>
2018-03-29 13:38:19 -0400

    fix install to make

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe objsrv::install_nco81
+# Inspec test for recipe objsrv::make_nco81
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
