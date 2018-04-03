f72ed1d476aeae3bda288ac0d5691db2b991566d
Ed Overton <e.overton@gmail.com>
2018-04-03 13:48:01 -0400

    move omni.dat

--- recipes/setup_nc_base.rb
+++ recipes/setup_nc_base.rb
@@ -1,28 +1,3 @@
-# set the IP and probe server name
-hostsfile_entry node['PAP'] do
-  hostname node['PA']
-  action   :create
-  unique   true
-end
-
-# create omni.dat via template
-template "#{node['objsrv']['nc_dir']}/etc/omni.dat" do
-  source 'omni.dat.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
-  mode 0664
-end
-
-# create the interface file
-execute 'create_interface' do
-  command "#{node['objsrv']['nc_dir']}/bin/nco_igen"
-  cwd "#{node['objsrv']['nc_dir']}/bin"
-  not_if { File.exist?("#{node['objsrv']['nc_dir']}/etc/interfaces.linux2x86") }
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
-  action :run
-end
-
 # create fips.conf file for TLS 1.2 security
 template "#{node['objsrv']['nc_dir']}/etc/security/fips.conf" do
   source 'fips.conf.erb'
--- recipes/setup_netcool.rb
+++ recipes/setup_netcool.rb
@@ -1,3 +1,28 @@
+# set the IP and probe server name
+hostsfile_entry node['PAP'] do
+  hostname node['PA']
+  action   :create
+  unique   true
+end
+
+# create omni.dat via template
+template "#{node['objsrv']['nc_dir']}/etc/omni.dat" do
+  source 'omni.dat.erb'
+  user node['objsrv']['nc_act']
+  group node['objsrv']['nc_grp']
+  mode 0664
+end
+
+# create the interface file
+execute 'create_interface' do
+  command "#{node['objsrv']['nc_dir']}/bin/nco_igen"
+  cwd "#{node['objsrv']['nc_dir']}/bin"
+  not_if { File.exist?("#{node['objsrv']['nc_dir']}/etc/interfaces.linux2x86") }
+  user node['objsrv']['nc_act']
+  group node['objsrv']['nc_grp']
+  action :run
+end
+
 # create configuration files for PA
 template "#{node['objsrv']['ob_dir']}/etc/#{node['objsrv']['os_pa_name']}.conf" do
   source 'nco_pa.conf.erb'
