35e60f6a8867c41e08c605b96fd8bbe3d8b5348a
Ed Overton <e.overton@gmail.com>
2018-09-11 13:44:24 -0400

    add x11

--- recipes/make_nc_os.rb
+++ recipes/make_nc_os.rb
@@ -2,7 +2,8 @@
 include_recipe '::nc_filesystem'
 # install the base Netcool software
 include_recipe 'nc_base::make_nc_base'
-# include_recipe 'nc_base::install_nckl'
+include_recipe 'nc_base::add_x11'
+include_recipe 'nc_base::install_nckl'
 # install the netcool gateways
 include_recipe '::install_gates'
 # setup and start Object server
