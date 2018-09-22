9c02894f8f380e063cde40cee56010c4c9bde57d
Ed Overton <e.overton@gmail.com>
2018-09-21 23:49:46 -0400

    tdw

--- recipes/setup_jdbc_gateway.rb
+++ recipes/setup_jdbc_gateway.rb
@@ -1,6 +1,6 @@
 # This will setup the gateways
 # create the jdbc Gateway dir
-directory node['objsrv']['rdy_dir'] do
+directory node['objsrv']['tdw_dir'] do
   user node['objsrv']['nc_act']
   group node['objsrv']['nc_grp']
   mode '0755'
@@ -10,9 +10,9 @@ end
 
 # copy jdbc template from default location
 execute 'copy_default_jdbc' do
-  command "find #{node['objsrv']['ob_dir']}/gates/bmc_jdbc -maxdepth 1 -type f -exec cp -t #{node['objsrv']['rdy_dir']} {} +"
+  command "find #{node['objsrv']['ob_dir']}/gates/jdbc -maxdepth 1 -type f -exec cp -t #{node['objsrv']['tdw_dir']} {} +"
   cwd '/usr/bin'
-  not_if { File.exist?("#{node['objsrv']['rdy_dir']}/bmc_jdbc.map") }
+  not_if { File.exist?("#{node['objsrv']['tdw_dir']}/tdw.map") }
   user node['objsrv']['nc_act']
   group node['objsrv']['nc_grp']
   action :run
@@ -20,22 +20,29 @@ end
 
 # create jdbc configuration files
 # map file
-template "#{node['objsrv']['rdy_dir']}/bmc_jdbc.map" do
-  source 'bmc_jdbc.map.erb'
+template "#{node['objsrv']['tdw_dir']}/tdw.map" do
+  source 'reporting.jdbc.map.erb'
   user node['objsrv']['nc_act']
   group node['objsrv']['nc_grp']
   mode 0444
 end
 # props file
-template "#{node['objsrv']['rdy_dir']}/G_BMC_REMEDY.props" do
-  source 'G_BMC_REMEDY.props.erb'
+template "#{node['objsrv']['tdw_dir']}/G_JDBC.props" do
+  source 'reporting.G_JDBC.props.erb'
   user node['objsrv']['nc_act']
   group node['objsrv']['nc_grp']
   mode 0444
 end
-# env file
-template "#{node['objsrv']['rdy_dir']}/nco_g_bmc_jdbc.env" do
-  source 'nco_g_bmc_jdbc.env.erb'
+# r/w file
+template "#{node['objsrv']['tdw_dir']}/tdw.rdrwtr.tblrep.def" do
+  source 'jdbc.rdrwtr.tblrep.def.erb'
+  user node['objsrv']['nc_act']
+  group node['objsrv']['nc_grp']
+  mode 0444
+end
+# command file
+template "#{node['objsrv']['tdw_dir']}/tdw.startup.cmd" do
+  source 'jdbc.startup.cmd.erb'
   user node['objsrv']['nc_act']
   group node['objsrv']['nc_grp']
   mode 0444
