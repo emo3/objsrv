5df49d90d1d5cb49087627e07d0dd2bfbf80bd59
Ed Overton <e.overton@gmail.com>
2020-05-18 00:19:00 -0400

    commit so far

--- recipes/install_gates.rb
+++ recipes/install_gates.rb
@@ -13,22 +13,31 @@
 end
 
 # cmd tool file
-a = []
+tcmd = []
+log '**** ^^^^ ####'
 %w(nco-g-jdbc nco-g-bmc-remedy).each do |cmd|
-  template "#{node['objsrv']['nc_home']}/ncprofile-c" do
-    source 'ncprofilec.erb'
-    variables(
-      procmd:  node['nc_tools'][cmd]['tool_cmd'],
-      pa_name: node['objsrv']['os_pa_name'],
-      nc_act:  node['objsrv']['nc_act'],
-      nc_pwd:  node['objsrv']['nc_pwd'],
-      prosrv:  node['nc_tools'][cmd]['tool_srv']
-    )
-    owner node['objsrv']['nc_act']
-    group node['objsrv']['nc_grp']
-    mode '0755'
-  end
+  t1 = node['nc_tools'][cmd].merge({'pa_name' => node['objsrv']['os_pa_name']})
+  log "2#{t1}"
+  # log "h1#{h1}"
+  t3 = {'nc_act' => node['objsrv']['nc_act']}
+  # h1 = t1[cmd].merge(t3)
+  log "3#{t3}"
+  t4 = {'nc_pwd' => node['objsrv']['nc_pwd']}
+  log "4#{t4}"
 end
+
+log "4#{tcmd}"
+template "#{node['objsrv']['nc_home']}/ncprofile-c" do
+  source 'ncprofilec.erb'
+  variables(
+    tools: tcmd
+  )
+#  sensitive true
+  owner node['objsrv']['nc_act']
+  group node['objsrv']['nc_grp']
+  mode '0755'
+end
+
 # %w(nco-g-jdbc nco-g-bmc-remedy).each do |cmd|
 #   execute "create-tool-cmd for #{cmd}" do
 #     command "echo #{node['nc_tools'][cmd]['tool_cmd']} >> #{node['nc_tools']['nc_home']}/ncprofile-c"
